f955a1b005cc1831689dfea4e02201c5c72ef7a9
hooy21c@gmail.com <hooy21c@gmail.com>
2019-11-28 17:59:33 +0900

    19-11-28

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,3 @@
-import { video } from "../db"
 import routes from "../routes"
 
 export const home = (req,res) => { 
--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const video = [
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+mongoose.connect(
+    "mongodb://localhost:27017/wetub", 
     {
-        id:324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1212,
-            name: "HoYeon",
-            email: "ho@yeon.com"
-        }
-    },
-    {
-        id:3293,
-        title: "test",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1212,
-            name: "HoYeon",
-            email: "ho@yeon.com"
-        }
-    },
-    {
-        id:593,
-        title: "dfasf",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1212,
-            name: "HoYeon",
-            email: "ho@yeon.com"
-        }
-    },
-    {
-        id:334393,
-        title: "fu",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1212,
-            name: "HoYeon",
-            email: "ho@yeon.com"
-        }
-    },
-]
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = () => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
