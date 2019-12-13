bcc0280b2744a4827afd2b317479e045d7749543
hooy21c@gmail.com <hooy21c@gmail.com>
2019-12-13 18:00:09 +0900

    19-12-13
    style

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middleware";
 
-const app = express() //application 생성
+const app = express(); //application 생성
 
 app.use(helmet());
-app.set('view engine', "pug");
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser()); //cookie를 전달받아 사용할 수 있도록 만들어주는 middleware.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true })); //사용자가 웹사이트로 전달하는 정보들을 검사 / request 정보에서 form이나 json 형태로 된 body를 검사.
+app.use(bodyParser.urlencoded({ extended: true })); //사용자가 웹사이트로 전달하는 정보들을 검사 / request 정보에서 form이나 json 형태로 된 body를 검사.
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
--- webpack.config.js
+++ webpack.config.js
@@ -7,26 +7,34 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
         test: /\.(scss)$/, //텍스트 파일을 추출하여 css 파일로 만들기
         use: ExtractCSS.extract([
           {
-            loader: "css-loader"
+            loader: "css-loader" //css를 가져옴
           },
           {
-            loader: "postcss-loader", //css를 받아서 plugin으로 css로 변환해줌
+            loader: "postcss-loader", //css를 받아서 plugin으로 css로 변환해줌 / 특정 plugin들을 css에 대해 실행시켜줌
             options: {
-              plugin() {
+              plugins() {
                 return [autoprefixer({ browsers: "cover 99.5%" })];
               }
             }
           },
           {
-            loader: "sass-loader" //아래에서 위로 실행되므로 마지막에 css-loader
+            loader: "sass-loader" //아래에서 위로 실행되므로 마지막에 css-loader / sass를 css로 옮겨줌
           }
         ])
       }
@@ -34,7 +42,7 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
+    filename: "[name].js"
   },
   plugins: [new ExtractCSS("styles.css")]
 };
