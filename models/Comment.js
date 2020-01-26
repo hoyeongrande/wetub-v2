4b1a9a714c118b1ae0eda4310333b6a7dd5f825c
hooy21c@gmail.com <hooy21c@gmail.com>
2020-01-26 20:28:34 +0900

    20-01-26

--- models/Comment.js
+++ models/Comment.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: "Text is required"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-}); 
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    types: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
 
 const model = mongoose.model("Comment", CommentSchema);
 export default model;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,16 +3,24 @@ import routes from "../routes";
 import {
   users,
   userDetail,
-  editProfile,
-  changePassword
+  getEditProfile,
+  postEditProfile,
+  changePassword,
+  getChangePassword,
+  postChangePassword
 } from "../controllers/userController";
 import { onlyPrivate } from "../middleware";
+import { postEditVideo } from "../controllers/videoController";
 
 const userRouter = express.Router();
 
 //userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, postEditProfile);
+
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
